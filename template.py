8421b15334a035c1421f83549682f8f7b09da4cf
Cedian03 <nyaalgotjohansson@gmail.com>
2024-12-05 10:24:21 +0100

    template bugfix

--- template.py
+++ template.py
@@ -2,8 +2,8 @@ from advent import BaseSolution
 
 
 class Solution(BaseSolution):
-    def part_one_impl(self) -> int:  # type: ignore
+    def part_one_impl(self, input: str) -> int:  # type: ignore
         raise NotImplementedError("Part one has not yet been implemented.")
 
-    def part_two_impl(self) -> int:  # type: ignore
+    def part_two_impl(self, input: str) -> int:  # type: ignore
         raise NotImplementedError("Part two has not yet been implemented.")
